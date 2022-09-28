775d2daa9d66d69227783abfe85e200b6a0b06e1
zhouhesheng <hzhou@maxleap.com>
2022-09-28 16:49:50 +0800

    webview and systray

--- cmd/demo/main.go
+++ cmd/demo/main.go
@@ -1,18 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
+	"github.com/getlantern/systray"
+	"github.com/getlantern/systray/example/icon"
 	"github.com/jchv/go-webview2"
 )
 
 func main() {
+	systray.Register(onReady, nil)
+
 	w := webview2.NewWithOptions(webview2.WebViewOptions{
 		Debug:     true,
 		AutoFocus: true,
 		WindowOptions: webview2.WindowOptions{
-			Title:  "Minimal webview example",
-			Width:  800,
+			Title:  "Webview2",
+			Width:  400,
 			Height: 600,
 			IconId: 2, // icon resource id
 			Center: true,
@@ -22,7 +27,28 @@ func main() {
 		log.Fatalln("Failed to load webview.")
 	}
 	defer w.Destroy()
-	w.SetSize(800, 600, webview2.HintFixed)
-	w.Navigate("https://en.m.wikipedia.org/wiki/Main_Page")
+	w.SetSize(400, 600, webview2.HintFixed)
+	w.Navigate("http://127.0.0.1:13001")
 	w.Run()
+}
+
+func onReady() {
+	systray.SetTemplateIcon(icon.Data, icon.Data)
+	systray.SetTitle("Webview example")
+	mShowLantern := systray.AddMenuItem("Show Lantern", "")
+	mShowWikipedia := systray.AddMenuItem("Show Wikipedia", "")
+	mQuit := systray.AddMenuItem("Quit", "Quit the whole app")
+	go func() {
+		for {
+			select {
+			case <-mShowLantern.ClickedCh:
+				fmt.Println("ClickedCh")
+			case <-mShowWikipedia.ClickedCh:
+				fmt.Println("ClickedCh")
+			case <-mQuit.ClickedCh:
+				systray.Quit()
+			}
+		}
+	}()
+
 }
